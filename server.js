19a44871b5811ad5828ef3844c396483df2e93ab
Josh Parker <jthomasparker@gmail.com>
2018-06-12 11:21:40 -0400

    set up basic files

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var PORT = process.env.PORT || 8080
+
+app.use(express.static('public'))
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+var routes = require('./routes.js')(app)
+
+
+app.listen(PORT, function(){
+    console.log("Server listening on port", PORT)
+})
